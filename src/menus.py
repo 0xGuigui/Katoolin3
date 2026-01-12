0db928ec7cd38a5998bc1972e2ca860444339a64
mflr0 <mflr0@onionmail.org>
2026-01-12 14:58:40 +0100

    Refactor help_menu and main_banner for improved readability and styling

--- src/menus.py
+++ src/menus.py
@@ -1,38 +1,39 @@
+from style import C_BLUE, C_GREEN, C_RED, C_RESET, print_header
+
 def print_separator():
     print("\n" + "-" * 64 + "\n")
 
 
+
 def help_menu():
-    print("****************** +Commands+ ******************\n")
-    print("\033[1;32mback\033[1;m \t\033[1;33mGo back\033[1;m")
-    print("\033[1;32mgohome\033[1;m\t\033[1;33mGo to the main menu\033[1;m")
-    print("\033[1;32mshell\033[1;m \t\033[1;33mOpen system shell\033[1;m")
-    print("\033[1;32mhelp\033[1;m \t\033[1;33mShow this help menu\033[1;m")
-    print("\033[1;32mExit\033[1;m\t\033[1;33mExit the script\033[1;m")
+    print_header("Commands")
+    print(f"{C_GREEN}back{C_RESET}   \tGo back")
+    print(f"{C_GREEN}gohome{C_RESET} \tGo to the main menu")
+    print(f"{C_GREEN}shell{C_RESET}  \tOpen system shell")
+    print(f"{C_GREEN}help{C_RESET}   \tShow this help menu")
+    print(f"{C_GREEN}exit{C_RESET}   \tExit the script")
 
 
 def main_banner():
-    banner = r"""
+    banner = fr"""
+{C_BLUE}
  $$\   $$\           $$\                         $$\ $$\            $$$$$$\  
  $$ | $$  |          $$ |                        $$ |\__|          $$ ___$$\ 
  $$ |$$  / $$$$$$\ $$$$$$\    $$$$$$\   $$$$$$\  $$ |$$\ $$$$$$$\  \_/   $$ |
- $$$$$  /  \____$$\\_$$  _|  $$  __$$\ $$  __$$\ $$ |$$ |$$  __$$\   $$$$$ / 
- $$  $$<   $$$$$$$ | \033[1;34mKali-Linux tools installer\033[1;m |$$ |$$ |$$ |  $$ |  \___$$\ 
- \033[1;34m$$ |\$$\ $$  __$$ | $$ |$$\ $$ |  $$ |$$ |  $$ |$$ |$$ |$$ |  $$ |$$\   $$ |
- $$ | \$$\\$$$$$$$  | \$$$$  |\$$$$$$  |\$$$$$$  |$$ |$$ |$$ |  $$ |\$$$$$$  |
- \__|  \__|\_______|  \____/  \______/  \______/ \__|\__|\__|  \__| \______/ V3.0\033[1;m
+ $$$$$  /  \____$$\_$$  _|  $$  __$$\ $$  __$$\ $$ |$$ |$$  __$$\   $$$$$ / 
+ $$  $$<   $$$$$$$ | {C_RED}Kali-Linux tools installer{C_BLUE} |$$ |$$ |$$ |  $$ |  \___$$\ 
+ $$ |\$$\ $$  __$$ | $$ |\$$\ $$ |  $$|$$ | $$ |$$ |$$ |$$ |  $$ |\$$\   $$ |
+ $$ | \$$\\$$$$$$$ | \$$$$  |\$$$$$$  |\$$$$$$  |$$ |$$ |$$ |  $$ |\$$$$$$  |
+ \__|  \__|\_______|  \____/  \______/  \______/ \__|\__|\__|  \__| \______/ V3.1
+{C_RESET}
 """
     print(banner)
+    print(f" {C_GREEN}+ -- -- +=[ Original Script by: LionSec | Homepage: www.neodrix.com {C_RESET}")
+    print(f" {C_GREEN}+ -- -- +=[ Rewrites and maintained by: 0xGuigui{C_RESET}")
+    print(f" {C_GREEN}+ -- -- +=[ Latest update: 1/12/2026{C_RESET}")
     print("")
-    print(" \033[1;32m+ -- -- +=[ Original Script by: LionSec | Homepage: www.neodrix.com \033[1;m")
-    print(" \033[1;32m+ -- -- +=[ Rewrites and maintained by: 0xGuigui\033[1;m")
-    print(" \033[1;32m+ -- -- +=[ Latest update: 1/12/2026\033[1;m")
-    print("")
-    print("")
-    print("\033[1;91m[W] Before updating and upgrading your system, please remove all Kali-linux repositories to "
-          "avoid any kind of problem.\033[1;m")
-    print("\033[1;91m[W] In some cases, Kali-Linux repositories can destabilize your system or worse, completely "
-          "destroy it.\033[1;m")
+    print(f"{C_RED}[W] Before updating and upgrading your system, please remove all Kali-linux repositories to avoid any kind of problem.{C_RESET}")
+    print(f"{C_RED}[W] In some cases, Kali-Linux repositories can destabilize your system or worse, completely destroy it.{C_RESET}")
     print("")
 
 
