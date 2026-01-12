327b81c452f0acd5707da92c83c1bc89f8677c46
mflr0 <mflr0@onionmail.org>
2026-01-12 14:58:26 +0100

    Refactor repo_menu and main functions to use print_menu for better code organization and user interaction

--- src/app.py
+++ src/app.py
@@ -6,6 +6,7 @@ from categories import run_categories_menu
 from commands import run_shell_capture
 from menus import CLASSICMENU_INFO, help_menu, main_banner, print_separator
 from repo import add_kali_key, remove_kali_repo, show_sources_list, write_kali_repo
+from style import print_menu, clear, wait_for_input
 
 
 def setup():
@@ -17,16 +18,15 @@ def setup():
     os.system("clear")
     return True
 
-
 def repo_menu():
     while True:
-        print("")
-        print("1) Add kali linux repositories")
-        print("2) Update")
-        print("3) Remove all kali linux repositories")
-        print("4) View the contents of sources.list file")
-        print("back) Go back")
-        print("")
+        options = [
+            ("1", "Add kali linux repositories"),
+            ("2", "Update"),
+            ("3", "Remove all kali linux repositories"),
+            ("4", "View the contents of sources.list file"),
+        ]
+        print_menu("Kali Linux Repositories", options, tools_mode=False)
         repo = input(
             "\033[1;32mWhat do you want to do ?> \033[1;m")
         if repo == "1":
@@ -34,18 +34,19 @@ def repo_menu():
                 print(
                     "\033[1;31m\nFailed to add Kali repository key.\n\033[1;m")
                 print_separator()
-                continue
-            repo_status = write_kali_repo()
-            if repo_status == "added":
-                print(
-                    "\033[1;32m\nKali repositories have been added.\n\033[1;m")
-            elif repo_status == "exists":
-                print(
-                    "\033[1;33m\nKali repositories are already present.\n\033[1;m")
             else:
-                print(
-                    "\033[1;31m\nFailed to add Kali repositories.\n\033[1;m")
-            print_separator()
+                repo_status = write_kali_repo()
+                if repo_status == "added":
+                    print(
+                        "\033[1;32m\nKali repositories have been added.\n\033[1;m")
+                elif repo_status == "exists":
+                    print(
+                        "\033[1;33m\nKali repositories are already present.\n\033[1;m")
+                else:
+                    print(
+                        "\033[1;31m\nFailed to add Kali repositories.\n\033[1;m")
+                print_separator()
+            wait_for_input()
         elif repo == "2":
             rc, output = run_shell_capture("apt-get update")
             if rc == 0 and "W:" not in output and "E:" not in output:
@@ -55,6 +56,7 @@ def repo_menu():
             else:
                 print("\033[1;31m\nUpdate failed.\n\033[1;m")
             print_separator()
+            wait_for_input()
         elif repo == "3":
             removed = remove_kali_repo()
             if removed:
@@ -64,6 +66,7 @@ def repo_menu():
                 print(
                     "\033[1;33m\nNo Kali repositories were found.\n\033[1;m")
             print_separator()
+            wait_for_input()
         elif repo == "back":
             return
         elif repo == "gohome":
@@ -74,25 +77,32 @@ def repo_menu():
         elif repo == "4":
             show_sources_list()
             print_separator()
+            wait_for_input()
         else:
             print(
                 "\033[1;31mSorry, that was an invalid command!\033[1;m")
+            wait_for_input()
 
 
 def main():
     try:
         if not setup():
             return
-        main_banner()
 
         while True:
-            print("1) Add Kali repositories & Update")
-            print("2) View Categories")
-            print("3) Install classicmenu indicator")
-            print("4) Install Kali menu")
-            print("5) Help")
-            print("")
-
+            # Clear manualy here so we can print banner on top
+            clear()
+            main_banner()
+            
+            options = [
+                ("1", "Add Kali repositories & Update"),
+                ("2", "View Categories"),
+                ("3", "Install classicmenu indicator"),
+                ("4", "Install Kali menu"),
+                ("5", "Help"),
+            ]
+            # Tell print_menu NOT to clear
+            print_menu("Main Menu", options, tools_mode=False, clear_screen=False)
             option0 = input("\033[1;36mkat > \033[1;m")
             if option0 == "exit" or option0 == "quit":
                 print("Shutdown requested...Goodbye...")
@@ -110,18 +120,23 @@ def main():
                         "add-apt-repository ppa:diesch/testing && apt-get update")
                     cmd = os.system(
                         "sudo apt-get install classicmenu-indicator")
+                wait_for_input()
             elif option0 == "help":
                 print("")
                 help_menu()
+                wait_for_input()
             elif option0 == "4":
                 repo = input(
                     "\033[1;32mDo you want to install Kali menu ? [y/n]> \033[1;m")
                 if repo == "y":
                     cmd1 = os.system("apt-get install kali-menu")
+                wait_for_input()
             elif option0 == "5":
                 help_menu()
+                wait_for_input()
             else:
                 print("\033[1;31mSorry, that was an invalid command!\033[1;m")
+                wait_for_input()
     except KeyboardInterrupt:
         print("Shutdown requested...Goodbye...")
     except Exception:
